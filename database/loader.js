c8bde435fb68fc1d7f4076ad461039a81910741c
psfonseka <psfonseka@gmail.com>
2019-09-18 11:37:33 -0400

    created simple csv loader

---
+++ database/loader.js
@@ -0,0 +1,20 @@
+const csv = require('csv');
+const path = require('path');
+const loader = csv(); 
+
+function featuresCSV(Fone, Ftwo, Fthree, Ffour) {
+    this.id = Fone;
+    this.styleId = Ftwo;
+    this.size = Fthree;
+    this.quantity = Ffour;
+}; 
+
+let featuresData = [];
+
+loader.from.path(path.join(__dirname,"files/test.csv")).to.array(function (data) {
+    for (var index = 0; index < data.length; index++) {
+        featuresData.push(new featuresCSV(data[index][0], data[index][1], data[index][2], data[index][3]));
+    }
+    console.log(featuresData);
+});
+console.log("loading");
