568e2068f5665fa0dbddec4c3885ddd908c3109f
psfonseka <psfonseka@gmail.com>
2019-09-19 11:38:45 -0400

    pyloaderRelateds works on small test

--- database/pyloadRelateds.py
+++ database/pyloadRelateds.py
@@ -2,26 +2,38 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 filepathR = os.path.join(dir_path,'files/test.csv')
 filepathW = os.path.join(dir_path,'transforms/testNew.csv')
-print(filepathR)
+#print(filepathR)
+
+
+def stringifyArr(arr):
+    string = "{"
+    for val in arr:
+        string = string + val + ","
+    string = string[:-1]
+    string = string + "}"
+    return string
 
 currentData = []
-currentId = "0"
+currentId = "1"
 with open(filepathR) as fpR, open(filepathW, "w") as fpW: #open the file to read and file to write to
+    line = fpR.readline()
+    fpW.write("product_id,relatedItems\n")
+    print("product_id,relatedItems")
     line = fpR.readline()
     while line: #loop line by line
         #print(line)
         split = line.split(",")
         if split[1] != currentId: #check if it's a new main product id
-            print(currentId + ":")
-            print(currentData)
+            fpW.write(currentId + ",\"" + stringifyArr(currentData) + "\"" + "\n")
+            print(currentId + ",\"" + stringifyArr(currentData) + "\"")
             currentId = split[1]
             currentData = []
         else:
             related = split[2].rstrip('\n') #remove \n
             currentData.append(related) 
-        fpW.write(line)
         line = fpR.readline()
-    print(currentId + ":")
-    print(currentData)
+    fpW.write(currentId + ",\"" + stringifyArr(currentData) + "\"")
+    print(currentId + ",\"" + stringifyArr(currentData) + "\"")
+
 
 #print((data))
