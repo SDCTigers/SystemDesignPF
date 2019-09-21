176218d07065b04a7b14b1a4b8a2c9b44f69e27d
psfonseka <psfonseka@gmail.com>
2019-09-21 10:37:46 -0400

    added products list route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,30 @@ app.get('/', (req, res) => {
     res.statusCode = 200;
     res.send('Hello World!')
 });
+
+app.get('/products/list', (req, res) => {
+    let time0 = new Date().getTime();
+    let page = req.query.page || 1;
+    let count = req.query.count || 5;
+    let start = (page-1)*count+1;
+    let end = parseInt(start)+parseInt(count);
+    let lPromises = [];
+    for(let i = start; i < end; i++) {
+        lPromises.push(db.any(`SELECT id, name, slogan, description, category, default_price FROM product WHERE id = ${i}`));
+    }
+    Promise.all(lPromises)
+        .then(array => {
+            results = []
+            for (let i = 0; i < array.length; i++) {
+                results.push(array[i][0]);
+            }
+            res.statusCode = 200;
+            res.send(results); 
+            let time1 = new Date().getTime();
+            console.log(time1-time0 , " milliseconds")
+        })
+});
+
 app.get('/products/:product_id', (req, res) => {
     let id = req.params.product_id;
     let start = new Date().getTime();
@@ -67,6 +91,33 @@ app.get('/products/:product_id/related', (req, res) => {
             let end = new Date().getTime();
             console.log(end-start , " milliseconds")
         })
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+    let id = req.params.product_id;
+    let start = new Date().getTime();
+    db.any(`SELECT * FROM product WHERE id = ${id}`)
+        .then(results => {
+            let mainData = results[0];
+            let styles = mainData.styles;
+            let sPromises = [];
+            for (let i = 0; i < styles.length; i++) {
+                sPromises.push(db.any(`SELECT * FROM styles WHERE id = ${styles[i]}`));
+            }
+            Promise.all(sPromises)
+                .then(array => {
+                    let styles = [];
+                    for (let i = 0; i < array.length; i++) {
+                        styles.push(array[i]);
+                    }
+                    res.statusCode = 200;
+                    res.send(styles);
+                    let end = new Date().getTime();
+                    console.log(end-start , " milliseconds")
+                })
+            
+        })
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
